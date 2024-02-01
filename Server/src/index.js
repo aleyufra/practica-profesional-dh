5b103edacb7eba47609672c5fba590bbeb4ef087
aleyufra <alejandromaryufra@gmail.com>
2024-02-01 17:10:00 -0300

    Se agrego List, Detail, Create y Update * Se cambio el mensaje en listen en el archivo index.js * Se sagregaron las rutas de get, post, y patch para los distintos metodos del controlador de aspirantes, todavia falta crear el Delete*

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,4 +1,4 @@
 const app = require('./app.js');
 
 const PORT = 3000;
-app.listen(3000, () => console.log(`Server listen on http://localhost:${PORT}`));
+app.listen(3000, () => console.log(`Server listen on PORT: ${PORT}`));
